eb87a4c72be659b1134414734ba48eb32d7e82ab
serverrepairman <gustjwkd1007@gmail.com>
2022-12-26 22:35:54 +0900

    update settings.py socialaccount

--- team8_server/settings.py
+++ team8_server/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'allauth.socialaccount.providers.naver',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.kakao',
