4215dad7cabc7bef91e0b7409bc534f8a5a9de48
bean109 <auddl1020@gmail.com>
2022-12-26 21:42:29 +0900

    signup field required

--- snu_student/serializers.py
+++ snu_student/serializers.py
@@ -14,3 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name', 'password']
+        extra_kwargs = {
+            'email': {'required': True},
+            'name': {'required': True}
+        }
