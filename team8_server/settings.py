4fc2403a9a1119eec60b437a946491c434d91346
serverrepairman <gustjwkd1007@gmail.com>
2022-12-30 16:41:02 +0900

    cors headers included

--- team8_server/settings.py
+++ team8_server/settings.py
@@ -62,7 +62,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'team8_server.urls'
 
