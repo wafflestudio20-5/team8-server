d80d8a4246bbbe2a5b0089406d4816c7a7020755
serverrepairman <gustjwkd1007@gmail.com>
2022-12-30 16:48:29 +0900

    update settings.py

--- team8_server/settings.py
+++ team8_server/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.kakao',
     'allauth.socialaccount.providers.github',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
