00a149f65adab0dba28e833849f8bc593fca6c8b
serverrepairman <gustjwkd1007@gmail.com>
2023-02-04 03:32:39 +0900

    update resetperiod.py. reset pending

--- team8_server/management/commands/resetperiod.py
+++ team8_server/management/commands/resetperiod.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db.models import Count, F
 
+from snu_course.models import Course
 from team8_server.constants import Periods
 
 
@@ -13,10 +14,17 @@ class Command(BaseCommand):
         state.save()
         self.stdout.write(self.style.SUCCESS('장바구니 전 상태로 변경되었습니다.'))
 
+    def reset_pending(self):
+        courses = Course.objects.filter(pending=True)
+        course_count = courses.count()
+        courses.update(pending=False)
+        self.stdout.write(self.style.SUCCESS(f"{course_count}개의 pending이 초기화되었습니다."))
+
     def handle(self, *args, **kwargs):
         from team8_server.models import ServerState
         state = ServerState.objects.get()
         self.change_period(state)
+        self.reset_pending()
 
         from snu_student.models import UserToCourse
 
